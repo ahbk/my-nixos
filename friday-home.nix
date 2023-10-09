5b5f907eaa99b77f619d2d4bfc61c9d6f6fe8d9b
Alexander Holmbäck <alexander.holmback@gmail.com>
2023-10-09 09:49:47 +0200

    hyprland tweaks

--- friday-home.nix
+++ friday-home.nix
@@ -9,7 +9,7 @@
     enable = true;
     settings = {
       main.term = "xterm-256color";
-      main.font = "Source Code Pro:size=8";
+      main.font = "Source Code Pro:size=10";
       main.include = "~/Desktop/nixos/foot/theme.ini";
       main.dpi-aware = "yes";
       mouse.hide-when-typing = "yes";
