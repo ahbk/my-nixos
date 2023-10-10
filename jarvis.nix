ced7fbdc1d728c414577b741993208cf2d9cbba1
Alexander Holmbäck <alexander.holmback@gmail.com>
2023-10-10 14:38:07 +0200

    nginx with https

--- jarvis.nix
+++ jarvis.nix
@@ -5,10 +5,40 @@
   ];
 
   networking.hostName = "jarvis";
-  services.openssh.enable = true;
   networking.firewall = {
     enable = true;
-    allowedTCPPorts = [ 8000 ];
+    allowedTCPPorts = [ 8000 80 443 ];
   };
+  services.openssh.enable = true;
+  services.nginx = {
+    enable = true;
+    virtualHosts."ahbk.ddns.net" = {
+      addSSL = true;
+      enableACME = true;
+      root = "/var/www/ahbk.ddns.net";
+    };
+  };
+  security.acme = {
+    acceptTerms = true;
+    defaults.email = "alxhbk@proton.me";
+  };
+  environment.systemPackages = with pkgs; [
+    iotop
+    hdparm
+    inadyn
+  ];
 
+  systemd.services.inadyn = {
+    enable = true;
+    description = "manage inadyn";
+    unitConfig = {
+      Type = "simple";
+      After = [ "network-online.target" ];
+      Requires = [ "network-online.target" ];
+    };
+    serviceConfig = {
+      ExecStart = "${pkgs.inadyn}/bin/inadyn --foreground";
+    };
+    wantedBy = [ "multi-user.target" ];
+  };
 }
