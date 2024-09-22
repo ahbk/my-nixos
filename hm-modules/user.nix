279bec065b52a05aede3202129bfe0986865946b
Alexander Holmbäck <alex@ahbk.se>
2024-09-22 20:12:31 +0200

    nix -> lix

--- hm-modules/user.nix
+++ hm-modules/user.nix
@@ -1,10 +1,11 @@
-{ config, lib, ... }:
+{ config, lib, pkgs, ... }:
 
 let
   inherit (lib)
     hm
     mkDefault
     mkEnableOption
+    mkForce
     mkIf
     mkOption
     types
@@ -23,8 +24,21 @@ in
 
   config = mkIf cfg.enable {
     programs.home-manager.enable = true;
-    nix.settings = {
-      use-xdg-base-directories = true;
+    nix = {
+      package = mkForce pkgs.lix;
+      settings = {
+        auto-optimise-store = false;
+        bash-prompt-prefix = "(nix:$name)\040";
+        experimental-features = [ "nix-command" "flakes" ];
+        extra-nix-path = "nixpkgs=flake:nixpkgs";
+        max-jobs = "auto";
+        substituters = [ "https://cache.nixos.org" "https://cache.lix.systems" ];
+        trusted-public-keys = [
+          "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+          "cache.lix.systems:aBnZUw8zA7H35Cz2RyKFVs3H4PlGTLawyY5KRbvJR8o="
+        ];
+        use-xdg-base-directories = true;
+      };
     };
     home = {
       username = cfg.name;
--- modules/nix.nix
+++ modules/nix.nix
@@ -5,18 +5,30 @@
   ...
 }:
 
+let
+  inherit (lib)
+    mkDefault
+    mkForce
+    ;
+in
 {
   nix = {
-    package = pkgs.nixFlakes;
+    package = mkDefault pkgs.lix;
     registry.nixpkgs.flake = inputs.nixpkgs;
     channel.enable = false;
     settings = {
-      use-xdg-base-directories = true;
-      nix-path = lib.mkForce "nixpkgs=/etc/nix/inputs/nixpkgs";
-      experimental-features = [
-        "nix-command"
-        "flakes"
+      auto-optimise-store = false;
+      bash-prompt-prefix = "(nix:$name)\040";
+      experimental-features = [ "nix-command" "flakes" ];
+      max-jobs = "auto";
+      nix-path = mkForce "nixpkgs=/etc/nix/inputs/nixpkgs";
+      substituters = [ "https://cache.nixos.org" "https://cache.lix.systems" ];
+      trusted-users = [ "alex" "@wheel" ];
+      trusted-public-keys = [
+        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+        "cache.lix.systems:aBnZUw8zA7H35Cz2RyKFVs3H4PlGTLawyY5KRbvJR8o="
       ];
+      use-xdg-base-directories = true;
     };
   };
   environment.etc."nix/inputs/nixpkgs".source = "${inputs.nixpkgs}";
