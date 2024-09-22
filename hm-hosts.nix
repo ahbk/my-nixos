a97774667ef0f63d322c46b6778a9b8aed38709c
Alexander Holmbäck <alex@ahbk.se>
2024-09-22 13:04:00 +0200

    add hm-host arch

--- hm-hosts.nix
+++ hm-hosts.nix
@@ -15,4 +15,20 @@
       };
     };
   };
+  "alex@arch" = {
+    system = "x86_64-linux";
+    stateVersion = "23.11";
+    my-nixos-hm = {
+      ide = {
+        enable = true;
+        name = "Alexander Holmbäck";
+        email = "alex@ahbk.se";
+      };
+      shell.enable = true;
+      user = {
+        enable = true;
+        name = "alex";
+      };
+    };
+  };
 }
