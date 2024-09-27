57c1556e81f7b1ee12a81f034c7c1f6e2d6f80a8
Alexander Holmbäck <alex@ahbk.se>
2024-09-27 22:05:34 +0200

    hm-shell: add direnv

--- hm-modules/shell.nix
+++ hm-modules/shell.nix
@@ -38,94 +38,103 @@ in
       which
     ];
 
-    programs.ssh = {
-      enable = true;
-    };
+    programs = {
 
-    programs.bash = {
-      enable = true;
+      bash = {
+        enable = true;
 
-      sessionVariables = {
-        PATH = "$HOME/.local/bin:$PATH";
-        PROMPT_COMMAND = "\${PROMPT_COMMAND:+$PROMPT_COMMAND; }history -a; history -c; history -r";
-        HISTTIMEFORMAT = "%y-%m-%d %T";
+        sessionVariables = {
+          PATH = "$HOME/.local/bin:$PATH";
+          PROMPT_COMMAND = "\${PROMPT_COMMAND:+$PROMPT_COMMAND; }history -a; history -c; history -r";
+          HISTTIMEFORMAT = "%y-%m-%d %T";
+        };
+
+        shellAliases = {
+          battery = "cat /sys/class/power_supply/BAT/capacity && cat /sys/class/power_supply/BAT/status";
+          f = "xdg-open \"$(fzf)\"";
+          l = "eza -la --icons=auto";
+          ll = "eza";
+          cd = "z";
+          grep = "grep --color=auto";
+          switch = "nixos-rebuild switch --use-remote-sudo --show-trace --print-build-logs --verbose";
+          needs-reboot =
+            let
+              booted = "<(readlink /run/booted-system/{initrd,kernel,kernel-modules})";
+              current = "<(readlink /run/current-system/{initrd,kernel,kernel-modules})";
+              diff = "$(diff ${booted} ${current})";
+            in
+            ''if [[ ${diff} ]] then echo "yes"; else echo "no"; fi'';
+        };
+
+        historyControl = [
+          "ignoredups"
+          "erasedups"
+          "ignorespace"
+        ];
+
+        shellOptions = [
+          "histappend"
+          "histverify"
+          "checkwinsize"
+          "extglob"
+          "globstar"
+          "checkjobs"
+        ];
+
+        initExtra = ''
+          pwu() {
+            bw unlock --raw > ~/.bwsession
+          }
+          pw() {
+            BW_SESSION=$(<~/.bwsession) bw get password $@ | wl-copy
+          }
+          d() {
+            ${getExe pkgs.wdiff} "$1" "$2" | ${getExe pkgs.colordiff}
+          }
+        '';
       };
 
-      shellAliases = {
-        battery = "cat /sys/class/power_supply/BAT/capacity && cat /sys/class/power_supply/BAT/status";
-        f = "xdg-open \"$(fzf)\"";
-        l = "eza -la --icons=auto";
-        ll = "eza";
-        cd = "z";
-        grep = "grep --color=auto";
-        switch = "nixos-rebuild switch --use-remote-sudo --show-trace --print-build-logs --verbose";
-        needs-reboot =
-          let
-            booted = "<(readlink /run/booted-system/{initrd,kernel,kernel-modules})";
-            current = "<(readlink /run/current-system/{initrd,kernel,kernel-modules})";
-            diff = "$(diff ${booted} ${current})";
-          in
-          ''if [[ ${diff} ]] then echo "yes"; else echo "no"; fi'';
+      direnv = {
+        enable = true;
+        nix-direnv.enable = true;
       };
 
-      historyControl = [
-        "ignoredups"
-        "erasedups"
-        "ignorespace"
-      ];
+      fzf = {
+        enable = true;
+      };
 
-      shellOptions = [
-        "histappend"
-        "histverify"
-        "checkwinsize"
-        "extglob"
-        "globstar"
-        "checkjobs"
-      ];
+      ssh = {
+        enable = true;
+      };
 
-      initExtra = ''
-        pwu() {
-          bw unlock --raw > ~/.bwsession
-        }
-        pw() {
-          BW_SESSION=$(<~/.bwsession) bw get password $@ | wl-copy
-        }
-        d() {
-          ${getExe pkgs.wdiff} "$1" "$2" | ${getExe pkgs.colordiff}
-        }
-      '';
-    };
+      tmux = {
+        enable = true;
+        terminal = "tmux-256color";
+        keyMode = "vi";
+        escapeTime = 10;
+        baseIndex = 1;
+        extraConfig = with colors; ''
+          set-option -ga terminal-features ',foot:RGB'
+          set-option -g status-right "#{user}@#{host}"
+          set -ga terminal-overrides ",256col:Tc"
+          set -ga update-environment TERM
+          set -ga update-environment TERM_PROGRAM
+          set -g allow-passthrough on
+          set -g status-bg "${bg-400}"
+          set -g status-fg "${fg-500}"
+          bind -T copy-mode-vi y send -X copy-pipe-and-cancel 'wl-copy'
+        '';
+      };
 
-    programs.yazi = {
-      enable = true;
-      enableBashIntegration = true;
-    };
+      yazi = {
+        enable = true;
+        enableBashIntegration = true;
+      };
 
-    programs.zoxide = {
-      enable = true;
-    };
+      zoxide = {
+        enable = true;
+      };
 
-    programs.fzf = {
-      enable = true;
-    };
-
-    programs.tmux = {
-      enable = true;
-      terminal = "tmux-256color";
-      keyMode = "vi";
-      escapeTime = 10;
-      baseIndex = 1;
-      extraConfig = with colors; ''
-        set-option -ga terminal-features ',foot:RGB'
-        set-option -g status-right "#{user}@#{host}"
-        set -ga terminal-overrides ",256col:Tc"
-        set -ga update-environment TERM
-        set -ga update-environment TERM_PROGRAM
-        set -g allow-passthrough on
-        set -g status-bg "${bg-400}"
-        set -g status-fg "${fg-500}"
-        bind -T copy-mode-vi y send -X copy-pipe-and-cancel 'wl-copy'
-      '';
     };
 
   };
