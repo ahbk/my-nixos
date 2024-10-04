7beb234f786a37f5cb4d6fc8076b96297a6f25ae
Alexander Holmbäck <alex@ahbk.se>
2024-10-04 22:20:33 +0200

    colorize man

--- hm-modules/shell.nix
+++ hm-modules/shell.nix
@@ -47,6 +47,8 @@ in
           PATH = "$HOME/.local/bin:$PATH";
           PROMPT_COMMAND = "\${PROMPT_COMMAND:+$PROMPT_COMMAND; }history -a; history -c; history -r";
           HISTTIMEFORMAT = "%y-%m-%d %T";
+          MANPAGER = "sh -c 'col -bx | bat -l man -p'";
+          MANROFFOPT = "-c";
         };
 
         shellAliases = {
