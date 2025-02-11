1e2c51fa38fc41ef452bd5f977c67e9662c668e2
Alexander Holmbäck <alex@ahbk.se>
2025-02-11 14:35:35 +0100

    mailserver: update

--- flake.nix
+++ flake.nix
@@ -13,7 +13,7 @@
     nixvim.url = "github:nix-community/nixvim";
     nixvim.inputs.nixpkgs.follows = "nixpkgs";
 
-    nixos-mailserver.url = "gitlab:ahbk/nixos-mailserver/relay-domains";
+    nixos-mailserver.url = "gitlab:ahbk/nixos-mailserver/relay";
     nixos-mailserver.inputs.nixpkgs.follows = "nixpkgs";
 
     sverigesval.url = "git+ssh://git@github.com/ahbk/sverigesval.org";
