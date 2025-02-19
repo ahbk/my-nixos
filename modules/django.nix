205dcf87c5d2bd5425ccb0fe3b7c4ef8f959a47a
Alexander Holmbäck <alex@ahbk.se>
2025-02-19 10:24:05 +0100

    django: add celery

--- modules/django.nix
+++ modules/django.nix
@@ -11,6 +11,7 @@ let
   inherit (lib)
     filterAttrs
     flatten
+    getExe
     mapAttrs
     mapAttrs'
     mapAttrsToList
@@ -80,19 +81,24 @@ let
 
   envs = mapAttrs (
     name: cfg:
-    (
-      lib'.mkEnv cfg.appname {
+    (lib'.mkEnv cfg.appname (
+      {
+        DB_NAME = cfg.appname;
+        DB_USER = cfg.appname;
+        DB_HOST = "/run/postgresql";
         DEBUG = "false";
         DJANGO_SETTINGS_MODULE = "app.settings";
         HOST = cfg.hostname;
+        LOG_LEVEL = "WARNING";
         SCHEME = if cfg.ssl then "https" else "http";
         SECRET_KEY_FILE = config.age.secrets."${cfg.appname}/secret-key".path;
         STATE_DIR = stateDir cfg.appname;
       }
       // (optionalAttrs cfg.celery.enable {
-        CELERY_BROKER_URL = "redis://localhost:${toString cfg.celery.port}/0";
+        CELERY_BROKER_URL = "redis://127.0.0.1:${toString cfg.celery.port}/0";
+        FLOWER_URL_PREFIX = "/flower";
       })
-    )
+    ))
   ) eachSite;
 
   bins = mapAttrs (
@@ -142,6 +148,8 @@ in
       groups.${cfg.appname} = { };
     }) eachSite;
 
+    my-nixos.postgresql = mapAttrs (name: cfg: { ensure = true; }) eachSite;
+
     systemd.tmpfiles.rules = flatten (
       mapAttrsToList (name: cfg: [
         "d '${stateDir cfg.appname}' 0750 ${cfg.appname} ${cfg.appname} - -"
@@ -164,6 +172,18 @@ in
           ${cfg.locationStatic} = {
             alias = "${(djangoPkgs cfg.appname).static}/";
           };
+        }
+        // optionalAttrs (cfg.celery.enable) {
+          "/auth" = {
+            recommendedProxySettings = true;
+            proxyPass = "http://localhost:${toString cfg.port}";
+          };
+          "/flower/" = {
+            proxyPass = "http://localhost:5555";
+            extraConfig = ''
+              auth_request /auth/;
+            '';
+          };
         };
     }) eachSite;
 
@@ -181,10 +201,19 @@ in
 
       "${cfg.appname}-celery" = mkIf cfg.celery.enable {
         description = "start ${cfg.appname}-celery";
-        after = [ "network-online.target" ];
         serviceConfig = {
-          Type = "forking";
-          ExecStart = "${(djangoPkgs cfg.appname).app}/bin/celery -A app worker -l notice";
+          ExecStart = "${(djangoPkgs cfg.appname).app}/bin/celery -A app worker -l warning";
+          User = cfg.appname;
+          Group = cfg.appname;
+          EnvironmentFile = envs.${cfg.appname};
+        };
+        wantedBy = [ "multi-user.target" ];
+      };
+
+      "${cfg.appname}-flower" = mkIf cfg.celery.enable {
+        description = "start ${cfg.appname}-flower";
+        serviceConfig = {
+          ExecStart = "${(djangoPkgs cfg.appname).app}/bin/celery -A app flower --port=5555";
           User = cfg.appname;
           Group = cfg.appname;
           EnvironmentFile = envs.${cfg.appname};
@@ -200,6 +229,35 @@ in
           User = cfg.appname;
           Group = cfg.appname;
           EnvironmentFile = envs.${cfg.appname};
+        };
+      };
+      "${cfg.appname}-pgsql-dump" = {
+        description = "dump a snapshot of the postgresql database";
+        serviceConfig = {
+          Type = "oneshot";
+          ExecStart = "${getExe pkgs.bash} -c '${pkgs.postgresql}/bin/pg_dump -U ${cfg.appname} ${cfg.appname} > ${stateDir cfg.appname}/dbdump.sql'";
+          User = cfg.appname;
+          Group = cfg.appname;
+        };
+      };
+      "${cfg.appname}-pgsql-restore" = {
+        description = "restore postgresql database from snapshot";
+        serviceConfig = {
+          Type = "oneshot";
+          ExecStart = "${getExe pkgs.bash} -c '${pkgs.postgresql}/bin/psql -U ${cfg.appname} ${cfg.appname} < ${stateDir cfg.appname}/dbdump.sql'";
+          User = cfg.appname;
+          Group = cfg.appname;
+        };
+      };
+    }) eachSite;
+
+    systemd.timers = lib'.mergeAttrs (name: cfg: {
+      "${cfg.appname}-pgsql-dump" = {
+        description = "Scheduled PostgreSQL database dump";
+        wantedBy = [ "timers.target" ];
+        timerConfig = {
+          OnCalendar = "daily";
+          Unit = "${cfg.appname}-pgsql-dump";
         };
       };
     }) eachSite;
